8229a5c02fcc5a88a53656792ee191db4a7cdf05
luis guilherme gomes <luisguilherme_pso@hotmail.com>
2019-12-30 19:27:20 -0400

    Refactoring 4

--- refactoring/src/refactoring/Customer.java
+++ refactoring/src/refactoring/Customer.java
@@ -35,10 +35,7 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
-
-            thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -48,20 +45,17 @@ public class Customer {
                 frequentRenterPoints++;
             }
 
-            //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t"
-                    + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
 
         }
-        //add footer lines
+
+        // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints)
                 + " frequent renter points";
         return result;
-    }
 
-    private double amountFor(Rental aRental) {
-      return aRental.getCharge();  
-   }
+    }
 }
